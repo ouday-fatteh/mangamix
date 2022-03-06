e14df0a585cdd7faec19b22eed81fea6ed828641
ouday-fatteh <oudayhacker007@gmail.com>
2022-03-06 13:51:32 +0100

    added slideshow

---
+++ client/src/components/HeroHome/Images.jsx
@@ -0,0 +1,24 @@
+import Image1 from  '../../images/herohome.jpg';
+import Image2 from  '../../images/onepiece.jpg';
+import Image3 from  '../../images/demonslayer.png';
+
+export const slideImages = [
+    {
+      url: Image1 ,
+      title: 'Naruto',
+      genre: 'Action,Drama - 2002 - 900 Chapters',
+      description : 'A young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.',
+    },
+    {
+      url: Image2,
+      title: 'One Piece',
+      genre: 'Action,Drama - 2009 - 200 Chapters',
+      description : 'Follows the adventures of Monkey D. Luffy and his pirate crew in order to find the greatest treasure ever left by the legendary Pirate, Gold Roger. '
+    },
+    {
+      url: Image3,
+      title: 'Demon Slayer',
+      genre: 'Action,Drama - 2014 - 120 Chapters',
+      description : 'The love and caring between Nejuko and Tanjiro is the main theme of the story. Tanjiro wants to save her sister and the only way to do that is to bring the blood of the person who changed her into Demon.'
+    },
+  ];
